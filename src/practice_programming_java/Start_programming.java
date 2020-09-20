e67b23c7db1261ca70bca0cda35495e611e63dca
k9want <zndwlrnjr26@daum.net>
2020-09-20 21:55:46 +0900

    Hello world 메시지 추가

--- src/practice_programming_java/Start_programming.java
+++ src/practice_programming_java/Start_programming.java
@@ -2,4 +2,11 @@ package practice_programming_java;
 
 public class Start_programming {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("start practice_programming_java");
+		System.out.println("Hello world");
+		System.out.println("...");
+	}
+
 }
