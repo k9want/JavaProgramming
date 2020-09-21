8463c680b80ac52cec995766392d2e767b8263d3
k9want <zndwlrnjr26@daum.net>
2020-09-21 17:29:45 +0900

    Collection ArrayList 연습 (length 메소드)

---
+++ src/ch7_collection/ArrayList_longestname.java
@@ -0,0 +1,37 @@
+package ch7_collection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayList_longestname {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> ArrayList_name = new ArrayList<String>();
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i=0; i<4; i++) {
+			System.out.print("이름을 입력하시요");
+			String str = scanner.nextLine();
+			ArrayList_name.add(str);
+		}
+		
+		for(int i=0; i<4; i++) {
+			
+			String name= ArrayList_name.get(i);
+			System.out.print(name+" ");
+			
+		}
+		
+		int longnameIndex =0;
+		for(int i=1; i<ArrayList_name.size(); i++) {
+			
+			if(ArrayList_name.get(longnameIndex).length() < ArrayList_name.get(i).length()) {
+				longnameIndex=i;
+			}	
+		}
+		System.out.println("가장 긴 이름은 : " + ArrayList_name.get(longnameIndex));
+		
+	}
+
+}
