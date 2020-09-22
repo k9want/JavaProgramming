ef17fcd3f19e34aa0665f0431f634c92d58d26fa
k9want <zndwlrnjr26@daum.net>
2020-09-22 20:58:02 +0900

    ArrayList<Character> 점수 변환하여 평균 출력

---
+++ src/ch7_collection/Ch7_p434_02.java
@@ -0,0 +1,44 @@
+package ch7_collection;
+
+import java.util.*;
+
+public class Ch7_p434_02 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner =new Scanner(System.in);
+		ArrayList<Character> grade = new ArrayList<Character>();
+		
+		System.out.print("6개의 학점을 빈 칸으로 분리 입력(A/B/C/D/F)>>");
+		for(int i=0; i<6; i++) {
+		char n = scanner.next().charAt(0);
+		grade.add(n);
+		
+		}
+		double sum = 0.0;
+		double avg;
+		for(int i =0; i<grade.size(); i++) {
+			switch(grade.get(i)) {
+			case 'A':
+				sum += 4.0;
+				break;
+			case 'B':
+				sum += 3.0;
+				break;
+			case 'C':
+				sum += 2.0;
+				break;
+			case 'D':
+				sum += 1.0;
+				break;
+			case 'F':
+				sum += 0;
+				break;
+			}
+		}
+		avg = sum / 6;
+		System.out.println(avg);
+	}
+
+}
