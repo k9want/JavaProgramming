e116ecde532a93d98291072dcd08c815d3a10404
k9want <zndwlrnjr26@daum.net>
2020-10-01 03:42:02 +0900

    상속 복습 원래하는 방식대로 하기!!!

--- src/ch5_inheritance/Ch5_p315_02.java
+++ src/ch5_inheritance/Ch5_p315_02.java
@@ -1,7 +1,5 @@
 package ch5_inheritance;
 
-
-
 class TV{
 	private int size;
 	public TV(int size) {this.size = size; }
@@ -26,15 +24,34 @@ class ColorTV extends TV{
 	}
 }
 
-
-
-public class Ch5_p314_01  {
+class IPTV extends ColorTV{
+	String IP;
+	public IPTV(String IP,int size,int color) {
+		super(size,color);
+		this.IP=IP;
 	
+	}
+	
+	String getIP() {
+		return IP;
+	}
+	
+	void printProperty() {
+		System.out.println("나의 IPTV는 "+getIP()+" 주소의"+getSize()+"인치 "+getColor()+"컬러");
+	}
+	
+}
+
+
+
+public class Ch5_p315_02 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		IPTV iptv = new IPTV("192.1.1.2", 32, 2048);
+		iptv.printProperty();
 
-		ColorTV myTV = new ColorTV(32,1024);
-		myTV.printProperty();
+	}
 
-}
 }
