d41171ef97ee19d0395d5d1884024b709689137f
k9want <zndwlrnjr26@daum.net>
2020-10-01 05:10:06 +0900

    ch5_상속연습

---
+++ src/ch5_inheritance/Ch5_p317_05.java
@@ -0,0 +1,61 @@
+package ch5_inheritance;
+
+class Point{
+	private int x,y;
+	Point(){
+		
+	}
+	Point(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
+	
+	void move(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+}
+
+class ColorPoint extends Point{
+	
+	String color;
+	public ColorPoint(){}
+	
+	ColorPoint(int x,int y, String color){
+	super(x,y);
+	this.color=color;
+	}
+	void setXY(int x, int y){
+		move(x,y);
+	}
+	void setColor(String color) {
+		this.color=color;
+	}
+	String getColor(){
+		return color;
+	}
+	public String toString(){
+		return getColor()+"색의 ("+getX()+","+getY()+")"+"의 점";
+	}
+	
+}
+
+public class Ch5_p317_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ColorPoint cp = new ColorPoint(5,5,"Yellow");
+		cp.setXY(10, 20);
+		cp.setColor("RED");
+		String str =cp.toString();
+		System.out.println(str + "입니다 ");
+	}
+
+}
