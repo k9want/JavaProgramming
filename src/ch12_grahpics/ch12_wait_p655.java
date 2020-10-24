09300da32f4831555187c5f7b5fc474cc770734b
k9want <zndwlrnjr26@daum.net>
2020-10-24 16:32:10 +0900

    JLabel에 paintComponent() 사용

---
+++ src/ch12_grahpics/ch12_wait_p655.java
@@ -0,0 +1,39 @@
+package ch12_grahpics;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ch12_wait_p655 extends JFrame {
+	Jla la2 = new Jla();
+
+	private ch12_wait_p655() {
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Jpanel에서 paintComponent() 사용해보기");
+
+		this.setContentPane(la2);
+
+		this.setSize(400, 300);
+		this.setVisible(true);
+
+	}
+
+	class Jla extends JLabel {
+
+		ImageIcon icon = new ImageIcon("images/cafe_map.png");
+		Image img = icon.getImage();
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(img, 20, 20, this);
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ch12_wait_p655();
+	}
+
+}
